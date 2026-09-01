7e165005c7562c89e9a6548c5b21b9fb5cd9d709
uylukkemiği <berkaybilgi2008@gmail.com>
2026-09-01 22:18:32 +0300

    Add per-book visual prefab reference to BookData

--- Assets/Scripts/BookData.cs
+++ Assets/Scripts/BookData.cs
@@ -3,9 +3,14 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "ComicShop/Book Data", fileName = "BookData")]
 public class BookData : ScriptableObject
 {
+    [Header("Kimlik")]
     [Min(0)]
     public int BookID;
 
     [Min(0)]
     public int BrandID;
+
+    [Header("Gorsel Prefab")]
+    [Tooltip("Bu kitabin fiziksel model prefab'i. Her farkli kitap kendi prefab'ini kullanabilir.")]
+    public GameObject bookPrefab;
 }
