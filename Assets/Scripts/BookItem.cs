fd2160b38190788bd567f7bbf042e1fe13df0c8d
uylukkemiği <berkaybilgi2008@gmail.com>
2026-09-20 21:50:01 +0300

    Use editable BookDisplayName for book labels

--- Assets/Scripts/BookItem.cs
+++ Assets/Scripts/BookItem.cs
@@ -7,7 +7,7 @@ public class BookItem : MonoBehaviour
     [Min(0)] public int bookID;
     [Min(0)] public int brandID;
 
-    public string DisplayName => $"Book {bookID + 1}";
+    public string DisplayName => GetComponent<BookDisplayName>()?.Name is string customName && !string.IsNullOrWhiteSpace(customName) ? customName : $"Book {bookID + 1}";
 
     [Header("Kenar (Outline) Highlight Ayarlari")]
     public Material outlineMaterial;
